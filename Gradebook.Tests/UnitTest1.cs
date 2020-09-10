9062ea34c1ef80741e167cdeca50d1357f276c53
Aman Jain <amanajain98@gmail.com>
2020-09-10 18:40:50 +0530

    Added Check Min Test in a Separate Unit Test file.

--- Gradebook.Tests/UnitTest1.cs
+++ Gradebook.Tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace Gradebook.Tests
 {
-    public class Tests
+    public partial class Tests
     {
         [SetUp]
         public void Setup()
---
+++ Gradebook.Tests/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook;
+using NUnit.Framework;
+
+namespace Gradebook.Tests
+{ 
+    public partial class Tests
+    {
+        [Test]
+        public void Test_to_checkMincomputation()
+        {
+            var testbook = new Book();
+            testbook.add("1618", 99.999);
+            testbook.add("1583", 99.99);
+            testbook.add("1585", 49.99);
+            double actualmin = testbook.findMin();
+
+            double expectedmin = Math.Round(49.99, 2);
+
+            Assert.AreEqual(expectedmin, actualmin, 0.001);
+        }
+    }
+}
